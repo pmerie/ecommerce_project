65989e17e98bc30769a23d1090e8caf895388057
pmerie <psantiago2@academic.rrc.ca>
2026-03-26 19:33:20 -0500

    fixed category issues
    added Horror + other categories
    corrected book categories

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,5 +1,22 @@
 ActiveAdmin.register Book do
   permit_params :title, :author, :description, :price, :stock, :category_id
+
+  # ✅ FORM (THIS FIXES YOUR ERROR)
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :author
+      f.input :description
+      f.input :price
+      f.input :stock
+
+      # ✅ Correct for your column: category_name
+      f.input :category, as: :select,
+              collection: Category.all.map { |c| [ c.category_name, c.id ] }
+    end
+    f.actions
+  end
+
   index do
     selectable_column
     id_column
@@ -7,19 +24,19 @@ ActiveAdmin.register Book do
     column :author
     column :price
     column :stock
+
     column :category do |book|
       book.category&.category_name
     end
+
     column "Actions" do |book|
       links = []
       links << link_to("View", admin_book_path(book))
       links << link_to("Edit", edit_admin_book_path(book))
-      # Use button_to for delete - this is the most reliable with Turbo
-      links << button_to("Delete", admin_book_path(book),
+      links << link_to("Delete", admin_book_path(book),
         method: :delete,
-        data: { confirm: "Are you sure you want to delete this book?" },
-        class: "text-red-600 hover:underline"   # makes it look more like a link
-      )
+        data: { turbo_method: :delete, turbo_confirm: "Are you sure?" })
+
       safe_join(links, " | ")
     end
   end
